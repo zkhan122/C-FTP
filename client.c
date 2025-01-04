cb512b7b47be8aea4b3bb0b856b2dd9830ec6886
Zayaan <zayaankashif@yahoo.com>
2025-01-04 19:34:37 +0000

    push changes

--- client.c
+++ client.c
@@ -6,9 +6,13 @@
 #include "./tCp.h"
 
 int main(void) {
-  int client_to_server_socket =  socketInitialization();
+  int client_to_server_socket =  socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (client_to_server_socket == -1) {
+    perror("Socket creation failed..");
+    return 1;
+  }
   char *ip_client_address = "127.0.0.1";
-  int port = 1024;
+  int port = 8080;
 
   struct sockaddr_in client_addr;
   socklen_t client_addr_size;
@@ -16,24 +20,52 @@ int main(void) {
 
   memset(&client_addr, '\0', sizeof(client_addr));
   client_addr.sin_family = AF_INET;
-  client_addr.sin_port = port;
+  client_addr.sin_port = htons(port);
   client_addr.sin_addr.s_addr = inet_addr(ip_client_address);
 
   while (1) {
-    connect(client_to_server_socket, (struct sockclient_addr*)&client_addr, sizeof(client_addr));
+    int connectionStatus = connect(client_to_server_socket, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    if (connectionStatus == -1) {
+      perror("Connection failed");
+      close(client_to_server_socket);
+      return 1;
+    }
+
     printf("SUCCESS: CONNECTED TO SERVER\n");
 
+    // sending message to server
     bzero(messageBuffer, 1024);
     strcpy(messageBuffer, "Hi! This is a response from the client");
     printf("Client: %s\n", messageBuffer);
-    send(client_to_server_socket, messageBuffer, strlen(messageBuffer), 0);
 
+    int sendStatus = send(client_to_server_socket, messageBuffer, strlen(messageBuffer), 0);
+    printf("Client-end send Status: %d\n", sendStatus);
+    if (sendStatus == -1) {
+      perror("Error whilst sending message");
+      close(client_to_server_socket);
+      return 1;
+    }
+
+    // receiving message from server
     bzero(messageBuffer, 1024);
-    recv(client_to_server_socket, messageBuffer, sizeof(messageBuffer), 0);
+    int clientBytesReceived = recv(client_to_server_socket, messageBuffer, sizeof(messageBuffer), 0);
+
+    if (clientBytesReceived == -1) {
+      perror("Error whilst receiving message");
+      close(client_to_server_socket);
+      return 1;
+
+    } else if (clientBytesReceived == 0) {
+      printf("Server closed the connection\n");
+      close(client_to_server_socket);
+      return 1;
+    }
     printf("Server: %s\n", messageBuffer);
 
-    close(client_to_server_socket);
-    printf("Disconnected from the server");
+    close(client_to_server_socket); // closing socket to disconnect
+    printf("Disconnected from the server\n");
+
+    sleep(1);
 
   }
   return 0;
--- server.c
+++ server.c
@@ -12,16 +12,16 @@
 #define MAX_MESSAGE_SIZE 256
 
 int main(void) {
-    char *ip_address = "127.0.0.c.1";
-    int port = 1024;
+    char *ip_address = "127.0.0.1";
+    int port = 8080;
     char messageBuffer[1024];
     struct sockaddr_in server_socket_addr; // initializing the server socket
     struct sockaddr_in client_socket_addr; // initializing the client socket
     server_socket_addr.sin_family = AF_INET;
     server_socket_addr.sin_port = htons(port); // htons used for endianess to store in big endian format
     server_socket_addr.sin_addr.s_addr = inet_addr(ip_address); // RUN THE SERVER ON THIS IP ADDRESS
-    int server_socket = socketInitialization();
-    int client_socket = socketInitialization();
+    int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // int client_socket = socketInitialization();
 
     // connecting to the remote server_socket_addr
     int connectionStatus = bind(server_socket, (struct sockaddr *)&server_socket_addr, sizeof(server_socket_addr));
@@ -33,33 +33,60 @@ int main(void) {
     }
 
     listen(server_socket, 5);
-    printf("Listening..\n");
+    printf("Server is listening..\n");
+
+    int connected = 0;
 
     // client connection
     while (1) {
       socklen_t client_len = sizeof(client_socket_addr);
-      int connection = accept(server_socket, (struct sockaddr *)&client_socket_addr, &client_len);
+      int client_socket = accept(server_socket, (struct sockaddr *)&client_socket_addr, &client_len);
 
-      printf("Connection Status: %d\n", connectionStatus);
-      if (connectionStatus == -1) {
+      printf("Connection Status: %d\n", client_socket);
+      if (client_socket == -1) {
         perror("Error");
         return 1;
       }
       else {
         puts("Connection successful");
+        connected = 1;
       }
-      bzero(messageBuffer, 1024); // places nbyte null bytes in string s
-      recv(client_socket, messageBuffer, sizeof(messageBuffer), 0);
-      printf("Client: (%s)\n", messageBuffer);
+      if (connected == 1) {
+        // receive message from client
+        bzero(messageBuffer, 1024); // places nbyte null bytes in string s
+        int serverBytesReceived = recv(client_socket, messageBuffer, sizeof(messageBuffer), 0);
 
-      bzero(messageBuffer, 1024);
-      strcpy(messageBuffer, "Hi! This is a response from the server");
-      printf("Server: %s\n", messageBuffer);
-      send(client_socket, messageBuffer, strlen(messageBuffer), 0);
+        if (serverBytesReceived == -1) {
+          perror("Error whilst receiving message");
+          close(client_socket);
+          return 1;
+
+        } else if (serverBytesReceived == 0) {
+          printf("Server closed the connection\n");
+          close(client_socket);
+          return 1;
+        }
+
+        printf("Client: (%s)\n", messageBuffer);
+
+        // send message to client
+        bzero(messageBuffer, 1024);
+        strcpy(messageBuffer, "Hi! This is a response from the server");
+        printf("Server: %s\n", messageBuffer);
+
+        int sendStatus = send(client_socket, messageBuffer, strlen(messageBuffer), 0);
+
+        printf("Server-end send Status: %d", sendStatus, "\n");
+        if (sendStatus == -1) {
+          perror("Error whilst sending message");
+          close(client_socket);
+          return 1;
+        }
 
 
-      close(socket);
-      printf("\n --> Client has disconnected\n\n");
+        close(client_socket); // closing the current client socket to allow server to accept new connections
+        printf("\n --> Client has disconnected\n\n");
+      }
     }
 
     return 0;
